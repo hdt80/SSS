3da8a64f89af624e4d0db2bfc00f15753acc69ba
oxiCODING <oxiCODING@gmail.com>
2015-04-20 21:21:20 -0700

    added setRotation method to Actor class

--- Projects/3d-engine/code/headers/actors/Actor.h
+++ Projects/3d-engine/code/headers/actors/Actor.h
@@ -35,7 +35,8 @@ namespace engine {
                 inline const glm::mat4 getTransformation() const { return _transform.getTransformation(); }
                 inline const glm::vec3 getPosition() const { return _transform.getPosition(); }
                 inline const glm::quat getRotation() const { return _transform.getRotation(); }
-                inline void  setPosition(const glm::vec3& position) { _transform.setPosition(position); } 
+                inline void  setPosition(const glm::vec3& position) { _transform.setPosition(position); }
+                inline void  setRotation(const glm::quat& rotation) { _transform.setRotation(rotation); }
                 inline void  move(const glm::vec3& delta) { _transform.translate(delta); }
                 inline void  rotate(const glm::vec3& axis, float theta) { _transform.rotate(axis, theta); }
 
