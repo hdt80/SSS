a90ee02aa4f757c10fd0dc12d8e525e052764c2b
Danila <danila.fedorin@gmail.com>
2015-05-13 16:15:08 -0700

    Finished tha minigame

--- Projects/Minigames/WiringMinigame/WiringMinigame.ino
+++ Projects/Minigames/WiringMinigame/WiringMinigame.ino
@@ -1,23 +1,32 @@
-//FYI IT doesn't work yet
-
-byte inIDS[] = {
- 0,
- 1,
- 2 
-};
-
+//The output pins. 
 byte outIDS[] = {
- 8,
- 9,
- 10 
+ 2,
+ 3,
+ 4 
 };
 
+//The pins that light up when the respective wire is correctly wired (outIDS[0] wired correctly lights up lightsIDS[0]);
+byte lightIDS[] = {
+ 5,
+ 6,
+ 7 
+};
+
+//The pins that wires will be plugged into.
+byte inIDS[] = {
+ 8,
+ 10,
+ 12 
+};
+
+//Placeholder array, filled when randomizing which wire each pin is connected to.
 byte connectIDS[] = {
  8,
  9,
  10 
 };
 
+//Magically randomizes the pin assignments.
 void rewire(){
  randomSeed(analogRead(0));
   randomSeed(analogRead(0));
@@ -35,20 +44,18 @@ void rewire(){
    usedUP[randomInt] = true;
    connectIDS[randomInt] = inIDS[ran];
    ++ran; 
-   String toPrint = "Wired " + String(inIDS[ran - 1]) + " to " + String(randomInt);
-   Serial.println(toPrint);
+   String toPrint = "Wired " + String(inIDS[ran - 1]) + " to " + String(outIDS[randomInt]);
   } 
 }
 
 boolean checkWire(byte wire){
     for(int i = 0; i < 3; i ++){
-       analogWrite(outIDS[i], 0); 
+       digitalWrite(outIDS[i], LOW); 
     }
     delay(200);
-    analogWrite(outIDS[wire], 1023);
+    digitalWrite(outIDS[wire], HIGH);
     delay(200);
-          Serial.println(analogRead(connectIDS[wire]));
-    if(analogRead(connectIDS[wire]) > 1000){
+    if(digitalRead(connectIDS[wire]) == HIGH){
        return true;
     }
     return false;
@@ -56,22 +63,37 @@ boolean checkWire(byte wire){
 
 void setup(){
   Serial.begin(9600);
+  Serial.write(0);
   for(int i = 0; i < 3; i ++){
        pinMode(outIDS[i], OUTPUT);
-    }
+       pinMode(lightIDS[i], OUTPUT);
+  }
+   for(int i = 0; i < 3; i ++){
+       pinMode(inIDS[i], INPUT);
+  }
     
-  //rewire();
+  rewire();
   
 }
 
 void loop(){
+  if(checkWire(0) && checkWire(1) && checkWire(2)){
+    Serial.write(1);
+  }
+  
   if(checkWire(0)){
-    Serial.println("Wire 0 is working!");
+    digitalWrite(lightIDS[0], HIGH);
+  } else {
+    digitalWrite(lightIDS[0], LOW);
   }
   if(checkWire(1)){
-    Serial.println("Wire 1 is working!");
+    digitalWrite(lightIDS[1], HIGH);
+  } else {
+     digitalWrite(lightIDS[1], LOW); 
   }
   if(checkWire(2)){
-    Serial.println("Wire 2 is working!");
+    digitalWrite(lightIDS[2], HIGH);
+  } else {
+     digitalWrite(lightIDS[2], LOW); 
   }
 }
