0bdcbb1a44bc1a8273b1801410a47fd416f0c7d5
unknown <270307@SHS-8D8A6898.beaverton.k12.or.us>
2015-05-14 10:15:17 -0700

    Fixed Switch Minigame
    It actually works now!

--- Projects/Minigames/StartEngineMinigame/StartEngineMinigame.ino
+++ Projects/Minigames/StartEngineMinigame/StartEngineMinigame.ino
@@ -16,63 +16,107 @@ int led2chosen = 0;
 int led3chosen = 0;
 int led4chosen = 0;
 
+int ledOrder[] = {
+  -1,
+  -1,
+  -1,
+  -1
+};
+
 void setup() {
   randomSeed(analogRead(5));
-  
+
   // put your setup code here, to run once:
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
-  pinMode(led4,OUTPUT);
-  
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+
   //pinMode(switch1,INPUT); //dangit
-  
+
   Serial.begin(9600);
+  // Randomize the led
+
+  rled = random(8, 12);
+  for (int i = 0; i < 4; i++) {
+    while ( find_index(ledOrder, 3, rled) == true ) {
+      rled = random(8, 12);
+    }
+    Serial.println("Setting array " + String(i) + " to " + String(rled));
+    ledOrder[i] = rled;
+  }
+
+  Serial.println("Random pattern chosen!");
   // digitalWrite(13,HIGH);
 }
 
+boolean find_index(int a[], int maxn, int value)
+{
+  int i;
+
+  for (i = 0; i < maxn; i++)
+  {
+    if (a[i] == value)
+    {
+      return (true);
+    }
+  }
+  Serial.println(String(value) + " not found");
+  return (false);
+
+}
+
+void nextmode() {
+  mode++;
+  rled = ledOrder[mode - 1];
+}
+
 void loop() {
-  if (mode == 0){
-    digitalWrite(led1,HIGH);
-    digitalWrite(led2,HIGH);
-    digitalWrite(led3,HIGH);
-    digitalWrite(led4,HIGH);
-    
+  if (mode == 0) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+
     // select a random LED to light up
-    
-    rled = random(8, 11);
-    
     mode = 1;
+    rled = ledOrder[mode - 1];
+
+
   }
 
-  if (analogRead(switch1) > 1020  && rled == led1){
-    Serial.println("LED 1 I CHOSE YOU!!!!");
-    led1chosen = led1;
-    while (rled == led1 && (rled == led1chosen || rled || led2chosen && rled || led3chosen || rled == led4chosen)) { rled = random(8, 11); }
+  if (analogRead(switch1) > 1020  && rled == led1) {
+    nextmode();
   }
-  if (analogRead(switch2) > 1020  && rled == led2){
-    led2chosen = led2;
-    while (rled == led2 && (rled == led1chosen || rled || led2chosen && rled || led3chosen || rled == led4chosen)) { rled = random(8, 11); }
+  if (analogRead(switch2) > 1020  && rled == led2) {
+    nextmode();
   }
-  if (analogRead(switch3) > 1020  && rled == led3){
-    led3chosen = led3;
-    while (rled == led3 && (rled == led1chosen || rled || led2chosen && rled || led3chosen || rled == led4chosen)) { rled = random(8, 11); }
+  if (analogRead(switch3) > 1020  && rled == led3) {
+    nextmode();
   }
-  if (analogRead(switch4) > 1020  && rled == led4){
-    led4chosen = led4;
-    while (rled == led4 && (rled == led1chosen || rled || led2chosen && rled || led3chosen || rled == led4chosen)) { rled = random(8, 11); }
+  if (analogRead(switch4) > 1020  && rled == led4) {
+    nextmode();
   }
-  
-  Serial.println(led1chosen);
-   Serial.println(led2chosen);
-    Serial.println(led3chosen);
-     Serial.println(led4chosen);
-  
-  if (mode == 1){
-    digitalWrite(rled,HIGH);
+
+
+  if (mode >= 1 && mode <= 4) {
+    digitalWrite(rled, HIGH);
     delay(500);
-    digitalWrite(rled,LOW);
+    digitalWrite(rled, LOW);
     delay(500);
   }
   
+  if (mode == 5){
+   Serial.println("SWITCH_MINIGAME_COMPLETE");
+   for (int i=8;i<12;i++){
+    digitalWrite(i,HIGH);
+   } 
+   delay(400);
+   for (int i=8;i<12;i++){
+    digitalWrite(i,LOW);
+   } 
+   delay(400);
+  }
+
 }
+
